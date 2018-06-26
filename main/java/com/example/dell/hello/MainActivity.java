876e998dd1d23a1d1147933760dcc7f7470ae0b6
rajatchhabras <rajat1678.cse16@chitkara.edu.in>
2018-06-26 18:32:37 +0530

    done

--- main/java/com/example/dell/hello/MainActivity.java
+++ main/java/com/example/dell/hello/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.dell.hello;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button =findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+                                      @Override
+                                      public void onClick(View view) {
+                               Toast.makeText(getApplicationContext(), "Button Clicked",Toast.LENGTH_SHORT).show();
+                                      }
+                                  });
+
        Log.d("ActivityState","OnCreate()");
     }
 
